20fae1620d6f02ba3b27560c31181e90ad68c6cc
bynaki <bynaki@icloud.com>
2023-11-19 01:31:18 +0900

    changed config.sh linking method

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,48 +1,42 @@
-set fish_greeting ""
+if status --is-login
+  set fish_greeting ""
 
-set -gx TERM xterm-256color
-set -gx EDITOR nvim
+  set -gx TERM xterm-256color
+  set -gx EDITOR nvim
 
 # XDG base directory
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_STATE_HOME $HOME/.local/state
+  set -gx XDG_DATA_HOME $HOME/.local/share
+  set -gx XDG_CONFIG_HOME $HOME/.config
+  set -gx XDG_STATE_HOME $HOME/.local/state
 
 # devenv project path
-set -gx DEVENV $HOME/devenv
+  set -gx DEVENV $HOME/devenv
 
 # user fish functions path
-set -g fish_function_path $fish_function_path $DEVENV/config/fish/functions
+  set -g fish_function_path $fish_function_path $DEVENV/config/fish/functions
 
 # settings for each os
-switch (uname)
-  case Darwin
-    source $DEVENV/config/fish/config-macos.fish
-  case Linux
-    source $DEVENV/config/fish/config-linux.fish
-  case '*'
-    source $DEVENV/config/fish/config-windows.fish
-end
+  switch (uname)
+    case Darwin
+      source $DEVENV/config/fish/config-macos.fish
+    case Linux
+      source $DEVENV/config/fish/config-linux.fish
+    case '*'
+      source $DEVENV/config/fish/config-windows.fish
+  end
 
-command -qv nvim && alias vim nvim
-alias tmux "tmux -2"
+  command -qv nvim && alias vim nvim
+  alias tmux "tmux -2"
 
 # call fish key binding function
-fish_user_key_bindings
+  fish_user_key_bindings
 
-cd ~
+  cd ~
 # use node.js
-if test -f $XDG_CONFIG_HOME/fish/completions/nvm.fish
-  nvm use # must be `.nvmrc`
-end
-clear
+  if test -f $XDG_CONFIG_HOME/fish/completions/nvm.fish
+    nvm use # must be `.nvmrc`
+  end
+  clear
 # run neofetch
-command -qv neofetch && neofetch
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/anaconda3/bin/conda
-    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+  command -qv neofetch && neofetch
 end
-# <<< conda initialize <<<
-
