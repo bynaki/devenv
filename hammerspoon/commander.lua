02faa54c76ab36e300acc89aca0123b5f7b92f98
bynaki <bynaki@icloud.com>
2023-12-27 10:24:37 +0900

    bug fixed key focus again

--- hammerspoon/commander.lua
+++ hammerspoon/commander.lua
@@ -43,8 +43,10 @@ end
 function KeyEvent:keyStroke(modifiers, key)
   self._evt:setFlags(modifiers)
   self._evt:setKeyCode(hs.keycodes.map[key])
-  self._evt:post(hs.application.frontmostApplication())
-  self.block = false
+  local focused = hs.window.focusedWindow()
+  self._evt:post(focused:application())
+  -- self.block = false
+  self.block = true
 end
 
 local enabledCmd = nil
